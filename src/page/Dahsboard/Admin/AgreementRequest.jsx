e4ddec855a87c5c6ddcf05e707a7a3c1a9b362ab
Muhammad Saikat <smsaikat000@gmail.com>
2023-11-27 10:42:16 +0600

    added agreements reject functionality

--- src/page/Dahsboard/Admin/AgreementRequest.jsx
+++ src/page/Dahsboard/Admin/AgreementRequest.jsx
@@ -34,57 +34,101 @@ const AgreementRequest = () => {
       room_id,
       user,
       userName,
-
       role: "member",
       status: "checked",
     };
     const res = await axiosInterceptor.patch(`handelAgreement/${_id}`, obj);
     toast("Agreement Accept successful");
   };
-  
+  const rejectAgreement = async (
+    apartment_image,
+    apartment_no,
+    block_name,
+    date,
+    floor_no,
+    rent,
+    room_id,
+    user,
+    userName,
+    _id
+  ) => {
+    const obj = {
+      apartment_image,
+      apartment_no,
+      block_name,
+      date,
+      floor_no,
+      rent,
+      room_id,
+      user,
+      userName,
+      role: "user",
+      status: "checked",
+    };
+    const res = await axiosInterceptor.patch(`handelAgreement/${_id}`, obj);
+    toast("Agreement Rejected");
+  };
+
   return (
     <div className="mt-20">
       <ToastContainer></ToastContainer>
+      {/* ---------------------------------- */}
+      <div className="mb-10 flex items-center gap-2">
+        <div className="flex items-center gap-1">
+          <h1 className="w-[20px] h-[20px] bg-green-500"></h1>
+          <h1 className="">Accept</h1>
+        </div>
+        <div className="flex items-center gap-1">
+          <h1 className="w-[20px] h-[20px] bg-red-500"></h1>
+          <h1 className="">Reject</h1>
+        </div>
+      </div>
+      {/* ---------------------------------- */}
       {agreements && (
         <div>
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-100">
               <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   User name
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   User email
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Floor No
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Rent
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Room No
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Block Name
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Agreement request date
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Accept
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
                   Reject
                 </th>
               </tr>
             </thead>
+            {/* ------------------------------- */}
             <tbody className="bg-white divide-y divide-gray-200">
               {agreements.map((item) => (
                 <tr
                   key={item._id}
                   className={`${
                     item.role === "member" && "bg-green-500 text-white"
+                  } ${
+                    item.role === "user" &&
+                    item.status === "checked" &&
+                    "bg-red-500 text-white"
                   }`}
                 >
                   <td className="px-6 py-4 whitespace-nowrap">
@@ -117,11 +161,40 @@ const AgreementRequest = () => {
                     }
                     className="px-6 py-4 whitespace-nowrap "
                   >
-                    <button className={`${item.role ==='member'?"":'bg-green-500 text-white p-1 rounded'}`} disabled={item.role === "member"}>Accept</button>
+                    <button
+                      className={`${
+                        item.role !== "member"
+                          ? "bg-red-500"
+                          : "bg-green-500 text-white p-1 rounded"
+                      }`}
+                      disabled={
+                        item.role === "member" ||
+                        (item.role === "user" && item.status === "checked")
+                      }
+                    >
+                      Accept
+                    </button>
                   </td>
                   <td className="px-6 py-4 whitespace-nowrap">
                     <button
-                      disabled={item.role === "member"}
+                      onClick={() =>
+                        rejectAgreement(
+                          item.apartment_image,
+                          item.apartment_no,
+                          item.block_name,
+                          item.date,
+                          item.floor_no,
+                          item.rent,
+                          item.room_id,
+                          item.user,
+                          item.userName,
+                          item._id
+                        )
+                      }
+                      disabled={
+                        item.role === "member" ||
+                        (item.role === "user" && item.status === "checked")
+                      }
                       className={`${
                         item.role === "member"
                           ? ""
